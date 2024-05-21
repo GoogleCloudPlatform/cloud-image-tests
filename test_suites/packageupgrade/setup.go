beea906f60bb223514d148b9faced3699f013628
James Jerger <35977367+jjerger@users.noreply.github.com>
2024-05-20 17:18:43 -0700

    Add setup.go to packageupgrade tests
    Forking package setup; modifying to run package installs off staging

--- test_suites/packageupgrade/setup.go
+++ test_suites/packageupgrade/setup.go
@@ -12,60 +12,33 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Package packagevalidation tests that the guest environment and other
+// Package packageupgrade tests that the guest environment and other
 // necessary packages are installed and configured correctly.
-package packagevalidation
+package packageupgrade
 
 import (
-	"github.com/GoogleCloudPlatform/cloud-image-tests"
+	imagetest "github.com/GoogleCloudPlatform/cloud-image-tests"
 	"github.com/GoogleCloudPlatform/cloud-image-tests/utils"
 )
 
 // Name is the name of the test package. It must match the directory name.
-var Name = "packagevalidation"
+var Name = "packageupgrade"
 
 // TestSetup sets up the test workflow.
 func TestSetup(t *imagetest.TestWorkflow) error {
-	vm1, err := t.CreateTestVM("installedPackages")
-	if err != nil {
-		return err
-	}
-	vm1.RunTests("TestStandardPrograms|TestGuestPackages|TestNTP")
-
-	// as part of the migration of the windows test suite, these vms
-	// are only used to run windows tests. The tests themselves
-	// have components which need to be run on different vms.
+	// These tests are against googet which is only used on Windows
 	if utils.HasFeature(t.Image, "WINDOWS") {
-		vm2, err := t.CreateTestVM("googetFunctionality")
+		vm1, err := t.CreateTestVM("installDrivers")
 		if err != nil {
 			return err
 		}
-		vm2.RunTests("TestGooGetInstalled|TestGooGetAvailable|TestSigned|TestRemoveInstall" +
-			"|TestPackagesInstalled|TestPackagesAvailable|TestPackagesSigned")
-		vm3, err := t.CreateTestVM("vm3")
+		vm1.RunTests("TestDriverUpgrade")
+
+		vm2, err := t.CreateTestVM("installPackages")
 		if err != nil {
 			return err
 		}
-		vm3.RunTests("TestRepoManagement")
-		vm4, err := t.CreateTestVM("repoManagement")
-		if err != nil {
-			return err
-		}
-		vm4.RunTests("TestNetworkDriverLoaded|TestDriversInstalled|TestDriversRemoved")
-		// the former windows_image_validation test suite tests are run by this VM.
-		// It may make sense to move some of these tests to other suites in the future.
-		vm5, err := t.CreateTestVM("drivers")
-		if err != nil {
-			return err
-		}
-		vm5.RunTests("TestAutoUpdateEnabled|TestNetworkConnecton|TestEmsEnabled" +
-			"|TestTimeZoneUTC|TestPowershellVersion|TestStartExe|TestDotNETVersion" +
-			"|TestServicesState|TestWindowsEdition|TestWindowsCore|TestServerGuiShell")
-		sysprepvm, err := t.CreateTestVM("gcesysprep")
-		if err != nil {
-			return err
-		}
-		sysprepvm.RunTests("TestGCESysprep")
+		vm2.RunTests("TestPackageUpgrade")
 	}
 	return nil
 }
