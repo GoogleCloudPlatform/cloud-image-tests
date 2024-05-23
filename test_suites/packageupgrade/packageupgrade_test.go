fd6cdc4f0ab12a6cf0642a730cf9f732520c3197
James Jerger <35977367+jjerger@users.noreply.github.com>
2024-05-23 16:52:24 -0700

    Testing googet update over googet reinstall
    Proof of concept to see if this is more viable

--- test_suites/packageupgrade/packageupgrade_test.go
+++ test_suites/packageupgrade/packageupgrade_test.go
@@ -2,8 +2,6 @@ package packageupgrade
 
 import (
 	"fmt"
-	"regexp"
-	"strings"
 	"testing"
 
 	"github.com/GoogleCloudPlatform/cloud-image-tests/utils"
@@ -28,7 +26,7 @@ func ChangeRepo(t *testing.T) {
 	utils.FailOnPowershellFail(command, "Error adding staging repo", t)
 }
 
-func TestDriverUpgrade(t *testing.T) {
+/*func TestDriverUpgrade(t *testing.T) {
 	utils.WindowsOnly(t)
 	ChangeRepo(t)
 	drivers := []string{
@@ -71,12 +69,19 @@ func TestDriverUpgrade(t *testing.T) {
 			}
 		}
 	}
-}
+}*/
 
 func TestPackageUpgrade(t *testing.T) {
 	utils.WindowsOnly(t)
 	ChangeRepo(t)
-	packages := []string{
+
+	command := fmt.Sprint("googet -noconfirm update")
+	output, err := utils.RunPowershellCmd(command)
+	if err != nil {
+		t.Fatalf("Error updating googet packages. Stderr: %s", output.Stderr)
+	}
+
+	/*packages := []string{
 		"certgen",
 		"googet",
 		//"google-cloud-ops-agent",
@@ -118,5 +123,5 @@ func TestPackageUpgrade(t *testing.T) {
 				t.Fatalf("Install of '%s' returned unexpected result: %s", agent, output.Stdout)
 			}
 		}
-	}
+	}*/
 }
