0ee4cf750e48d5d20064cbf079c3bf251e8db4f8
Amelia Crate <acrate@google.com>
2024-07-10 09:43:07 -0700

    Don't test suspend/resume on ubuntu 2404 lts
    PiperOrigin-RevId: 651053810

--- test_suites/suspendresume/setup.go
+++ test_suites/suspendresume/setup.go
@@ -28,7 +28,7 @@ var Name = "suspendresume"
 
 // TestSetup sets up the test workflow.
 func TestSetup(t *imagetest.TestWorkflow) error {
-	if !strings.Contains(t.Image.Name, "rhel-8-2-sap") && !strings.Contains(t.Image.Name, "rhel-8-1-sap") && !strings.Contains(t.Image.Name, "debian-10") && !strings.Contains(t.Image.Family, "ubuntu-pro-1804-lts-arm64") {
+	if !strings.Contains(t.Image.Name, "rhel-8-2-sap") && !strings.Contains(t.Image.Name, "rhel-8-1-sap") && !strings.Contains(t.Image.Name, "debian-10") && !strings.Contains(t.Image.Family, "ubuntu-pro-1804-lts-arm64") && !strings.Contains(t.Image.Family, "ubuntu-2404-lts") {
 		suspend := &daisy.Instance{}
 		suspend.Scopes = append(suspend.Scopes, "https://www.googleapis.com/auth/cloud-platform")
 		suspendvm, err := t.CreateTestVMMultipleDisks([]*compute.Disk{{Name: "suspend"}}, suspend)
