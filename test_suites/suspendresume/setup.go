fd6afb396e83a00f0b150f6717ba4461a28aef59
Liam Leahy <liamll@google.com>
2024-08-20 11:07:30 -0700

    Skip Windows Server 2025 and Windows 11 24H2 in suspend/resume test suite.
    PiperOrigin-RevId: 665441543

--- test_suites/suspendresume/setup.go
+++ test_suites/suspendresume/setup.go
@@ -28,7 +28,7 @@ var Name = "suspendresume"
 
 // TestSetup sets up the test workflow.
 func TestSetup(t *imagetest.TestWorkflow) error {
-	if !strings.Contains(t.Image.Name, "rhel-8-2-sap") && !strings.Contains(t.Image.Name, "rhel-8-1-sap") && !strings.Contains(t.Image.Name, "debian-10") && !strings.Contains(t.Image.Family, "ubuntu-pro-1804-lts-arm64") && !strings.Contains(t.Image.Family, "ubuntu-2404-lts") {
+	if !strings.Contains(t.Image.Name, "windows-server-2025") && !strings.Contains(t.Image.Name, "windows-11-24h2") && !strings.Contains(t.Image.Name, "rhel-8-2-sap") && !strings.Contains(t.Image.Name, "rhel-8-1-sap") && !strings.Contains(t.Image.Name, "debian-10") && !strings.Contains(t.Image.Family, "ubuntu-pro-1804-lts-arm64") && !strings.Contains(t.Image.Family, "ubuntu-2404-lts") {
 		suspend := &daisy.Instance{}
 		suspend.Scopes = append(suspend.Scopes, "https://www.googleapis.com/auth/cloud-platform")
 		suspendvm, err := t.CreateTestVMMultipleDisks([]*compute.Disk{{Name: "suspend"}}, suspend)
