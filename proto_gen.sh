19566fba2ff64104955a7b59a63db2ea6b3770bd
Amelia Crate <acrate@google.com>
2024-08-22 13:32:42 -0700

    Document proto_gen requirements instead of modifying the environment
    PiperOrigin-RevId: 666470058

--- proto_gen.sh
+++ proto_gen.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+# You need to install the proto compiler (protobuf-compiler OS package on
+# Debian), and protoc-gen-go for go codegen.
+# google.golang.org/protobuf/cmd/protoc-gen-go or protoc-gen-go OS package on
+# Debian.
 
 # Installation path of protoc-gen-go must be in your $PATH for the protoc to 
 # find it. Usually protoc-gen-go is installed in $GOPATH/bin or $HOME/go/bin
-sudo apt install -y protobuf-compiler
-go install google.golang.org/protobuf/cmd/protoc-gen-go@latest
 protoc --go_out=. vm_test_info.proto
