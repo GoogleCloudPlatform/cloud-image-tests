f1450bddf12b266d55a0a73b8a586d31c02aa37d
Guest OS Images <guest-os-team@google.com>
2026-05-28 10:26:22 -0700

    Create validation test suites for customer customized images.
    PiperOrigin-RevId: 922848139

---
+++ test_suites/imagebuilder/setup.go
@@ -0,0 +1,20 @@
+// Package imagebuilder is a CIT suite for testing customer images built by the GCE Image Builder.
+package imagebuilder
+
+import (
+	"github.com/GoogleCloudPlatform/cloud-image-tests"
+)
+
+// Name is the name of the test package. It must match the directory name.
+var Name = "imagebuilder"
+
+// TestSetup sets up the test workflow.
+func TestSetup(t *imagetest.TestWorkflow) error {
+	vm1, err := t.CreateTestVM("networkinterfacesup")
+	if err != nil {
+		return err
+	}
+	vm1.RunTests("TestNetworkInterfacesUp")
+
+	return nil
+}
